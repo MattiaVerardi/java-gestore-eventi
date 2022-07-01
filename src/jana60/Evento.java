5850d0270aa948a2519ae466edc87253d7231bac
MattiaVerardi <verardi.mattia97@gmail.com>
2022-07-01 10:15:26 +0200

    consegna MIlestone1

---
+++ src/jana60/Evento.java
@@ -0,0 +1,101 @@
+package jana60;
+
+import java.time.LocalDate;
+
+/**
+ * 
+ * La consegna è di creare una classe Evento che abbia le seguenti proprietà: -
+ * titolo - data - numero di posti totali (cioè la capienza della location) -
+ * numero di posti prenotati Quando si istanzia un nuovo evento questi attributi
+ * devono essere tutti valorizzati nel costruttore, tranne posti prenotati che
+ * va inizializzato a 0. Inserire il controllo che la data non sia già passata e
+ * che il numero di posti totali sia un numero positivo. In caso contrario
+ * sollevare opportune eccezioni. Aggiungere metodi getter e setter in modo che:
+ * - titolo sia in lettura e in scrittura - data sia in lettura e scrittura -
+ * numero di posti totali sia solo in lettura - numero di posti prenotati sia
+ * solo in lettura Vanno inoltre implementati dei metodi public che svolgono le
+ * seguenti funzioni: 1. prenota: aggiunge uno ai posti prenotati. Se l’evento è
+ * già passato o non ha più posti disponibili deve sollevare un’eccezione. 2.
+ * disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci
+ * sono prenotazioni deve sollevare un’eccezione. 3. l’override del metodo
+ * toString() in modo che venga restituita una stringa contenente: data
+ * formattata - titolo Aggiungere eventuali metodi (public e private) che vi
+ * aiutino a svolgere le funzioni richieste.
+ *
+ */
+
+public class Evento {
+
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+
+	public Evento(String titolo, LocalDate data, int postiTotali, int postiPrenotati) throws Exception {
+		if (data.isAfter(LocalDate.now())) {
+			throw new Exception("Errore nell'inserimento della data");
+		}
+		if (!(postiTotali > 0)) {
+			throw new Exception("Il numero di posti inserito è errato");
+		}
+
+		this.titolo = titolo;
+		this.data = data;
+		this.postiTotali = postiTotali;
+		this.postiPrenotati = 0;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+
+	/**
+	 * Vanno inoltre implementati dei metodi public che svolgono le seguenti
+	 * funzioni: 1. prenota: aggiunge uno ai posti prenotati. Se l’evento è già
+	 * passato o non ha più posti disponibili deve sollevare un’eccezione. 2.
+	 * disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci
+	 * sono prenotazioni deve sollevare un’eccezione. 3. l’override del metodo
+	 * toString() in modo che venga restituita una stringa contenente: data
+	 * formattata - titolo Aggiungere eventuali metodi (public e private) che vi
+	 * aiutino a svolgere le funzioni richieste.
+	 */
+
+	public int prenota() throws Exception {
+		if (data.isAfter(LocalDate.now()) || !(postiPrenotati < postiTotali)) {
+			throw new Exception("Attenzione, errore nell'inserimento della data o nell'operazione di prenotazione");
+		}
+		return postiPrenotati += 1;
+	}
+
+	public int disdici() throws Exception {
+		if (data.isAfter(LocalDate.now()) || !(postiPrenotati > 0)) {
+			throw new Exception("Attenzione, errore nell'inserimento della data o nell'operazione di disdetta");
+		}
+		return postiPrenotati -= 1;
+	}
+
+	@Override
+	public String toString() {
+		return data + titolo;
+	}
+}
